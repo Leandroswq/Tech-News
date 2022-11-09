7f8b6af54f7c411c428cde64c20af597bbba5850
Leandroswq <leandrobonfim15@hotmail.com>
2022-11-09 16:55:48 -0300

    feat: Busca das top 5 noticias mais comentadas

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,15 @@
+from tech_news.helpers.format_news import get_title_and_url
+from tech_news.database import search_first__news_sorted
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    query = {}
+    query_sort = ("comments_count", -1)
+    noticias = search_first__news_sorted(query, query_sort, 5)
+    response = get_title_and_url(noticias)
+    return response
 
 
 # Requisito 11
