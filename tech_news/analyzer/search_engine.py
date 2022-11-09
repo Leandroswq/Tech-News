9675979ac46531b435b09e0d36398be1da59a1a8
Leandroswq <leandrobonfim15@hotmail.com>
2022-11-09 13:24:34 -0300

    feat: Busca da noticia pela data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -13,6 +14,16 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d").date()
+    except ValueError:
+        raise ValueError("Data inválida")
+    date = date.strftime("%d/%m/%Y")
+    query = {"timestamp": date}
+    noticias = search_news(query)
+    response = [(noticia["title"], noticia["url"]) for noticia in noticias]
+
+    return response
 
 
 # Requisito 8
